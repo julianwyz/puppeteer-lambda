e62a1b0301e11006e2255843b7004284f98b8a4b
Julian Wyzykowski <julian.wyzykowski@icloud.com>
2018-11-13 13:14:29 -0500

    change wrapper to be a class to encapsulate instance and fix potential bug that leads to an infinite loop when deployed to lambda

--- src/index.js
+++ src/index.js
@@ -1,127 +1,116 @@
-const aws = require('aws-sdk');
-const s3 = new aws.S3({apiVersion: '2006-03-01'});
-const fs = require('fs');
-const tar = require('tar');
-const Promise = require('bluebird');
-const puppeteer = require('puppeteer');
-const config = require('./config');
+const aws = require("aws-sdk");
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+const fs = require("fs");
+const tar = require("tar");
+const Promise = require("bluebird");
+const puppeteer = require("puppeteer");
+const config = require("./config");
 
-let globalBrowser = null;
-let getting = false;
-const _getBrowser = async (options) => {
-    if (null !== globalBrowser && await isBrowserAvailable()) {
-        return globalBrowser;
-    }
-    if (null === globalBrowser && !getting) {
-        getting = true;
-        if (process.env.CUSTOM_CHROME || (process.env.CHROME_BUCKET && process.env.CHROME_KEY)) {
-            await setupChrome();
-            globalBrowser = await puppeteer.launch(Object.assign({
-                headless: true,
-                executablePath: config.executablePath,
-                args: config.launchOptionForLambda,
-                dumpio: !!exports.DEBUG,
-                ignoreHTTPSErrors: true
-            }, options));
-        } else {
-            globalBrowser = await puppeteer.launch(Object.assign({
-                dumpio: !!exports.DEBUG,
-                ignoreHTTPSErrors: true
-            }, options));
-        }
-
-        const version = await globalBrowser.version();
-        console.log(`Launch chrome: ${version}`);
-        return globalBrowser;
-        // debugLog(async (b) => `launch done: ${await globalBrowser.version()}`);
-    } else {
-        do {
-            await Promise.delay(50);
-        } while (!globalBrowser);
-        return globalBrowser;
-    }
+function PuppeteerLambda() {
+  this.browser = null;
 }
 
-exports.getBrowser = async options => {
-    await _getBrowser(options);
-    return globalBrowser;
-};
-
-const isBrowserAvailable = async () => {
-    try {
-        await globalBrowser.version();
-    } catch (e) {
-        globalBrowser = null;
-        debugLog(e); // not opened etc.
-        return false;
+PuppeteerLambda.prototype.getBrowser = async function(options) {
+  if (this.browser !== null && (await this.isBrowserAvailable())) {
+    return this.browser;
+  } else {
+    if (
+      process.env.CUSTOM_CHROME ||
+      (process.env.CHROME_BUCKET && process.env.CHROME_KEY)
+    ) {
+      await this.setupChrome();
+      this.browser = await puppeteer.launch(
+        Object.assign(
+          {
+            headless: true,
+            executablePath: config.executablePath,
+            args: config.launchOptionForLambda,
+            dumpio: !!exports.DEBUG,
+            ignoreHTTPSErrors: true
+          },
+          options
+        )
+      );
+    } else {
+      this.browser = await puppeteer.launch(
+        Object.assign(
+          {
+            dumpio: !!exports.DEBUG,
+            ignoreHTTPSErrors: true
+          },
+          options
+        )
+      );
     }
-    return true;
+
+    const version = await this.browser.version();
+    console.log(`Launch chrome: ${version}`);
+    return this.browser;
+  }
 };
 
-const setupChrome = async () => {
-    if (!await existsExecutableChrome()) {
-        if (await existsLocalChrome()) {
-            debugLog('setup local chrome');
-            await setupLocalChrome();
-        } else {
-            debugLog('setup s3 chrome');
-            await setupS3Chrome();
-        }
-        debugLog('setup done');
+PuppeteerLambda.prototype.isBrowserAvailable = async function() {
+  try {
+    await this.browser.version();
+  } catch (e) {
+    this.browser = null;
+    return false;
+  }
+  return true;
+};
+PuppeteerLambda.prototype.setupChrome = async function() {
+  if (!(await this.existsExecutableChrome())) {
+    if (await this.existsLocalChrome()) {
+      await this.setupLocalChrome();
+    } else {
+      await this.etupS3Chrome();
     }
+  }
 };
-
-const existsLocalChrome = () => {
-    return new Promise((resolve, reject) => {
-        fs.exists(config.localChromePath, (exists) => {
-            resolve(exists);
-        });
+PuppeteerLambda.prototype.existsLocalChrome = async function() {
+  return new Promise((resolve, reject) => {
+    fs.exists(config.localChromePath, exists => {
+      resolve(exists);
     });
+  });
 };
-
-const existsExecutableChrome = () => {
-    return new Promise((resolve, reject) => {
-        fs.exists(config.executablePath, (exists) => {
-            resolve(exists);
-        });
+PuppeteerLambda.prototype.existsExecutableChrome = async function() {
+  return new Promise((resolve, reject) => {
+    fs.exists(config.executablePath, exists => {
+      resolve(exists);
     });
+  });
+};
+PuppeteerLambda.prototype.setupLocalChrome = async function() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(config.localChromePath)
+      .on("error", err => reject(err))
+      .pipe(
+        tar.x({
+          C: config.setupChromePath
+        })
+      )
+      .on("error", err => reject(err))
+      .on("end", () => resolve());
+  });
+};
+PuppeteerLambda.prototype.setupS3Chrome = async function() {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: config.remoteChromeS3Bucket,
+      Key: config.remoteChromeS3Key
+    };
+    s3.getObject(params)
+      .createReadStream()
+      .on("error", err => reject(err))
+      .pipe(
+        tar.x({
+          C: config.setupChromePath
+        })
+      )
+      .on("error", err => reject(err))
+      .on("end", () => resolve());
+  });
 };
 
-const setupLocalChrome = () => {
-    return new Promise((resolve, reject) => {
-        fs.createReadStream(config.localChromePath)
-            .on('error', (err) => reject(err))
-            .pipe(tar.x({
-                C: config.setupChromePath,
-            }))
-            .on('error', (err) => reject(err))
-            .on('end', () => resolve());
-    });
-};
-
-const setupS3Chrome = () => {
-    return new Promise((resolve, reject) => {
-        const params = {
-            Bucket: config.remoteChromeS3Bucket,
-            Key: config.remoteChromeS3Key,
-        };
-        s3.getObject(params)
-            .createReadStream()
-            .on('error', (err) => reject(err))
-            .pipe(tar.x({
-                C: config.setupChromePath,
-            }))
-            .on('error', (err) => reject(err))
-            .on('end', () => resolve());
-    });
-};
-
-const debugLog = (log) => {
-    if (config.DEBUG) {
-        let message = log;
-        if (typeof log === 'function') message = log();
-        Promise.resolve(message).then(
-            (message) => console.log(message)
-        );
-    }
-};
+module.exports = PuppeteerLambda;
